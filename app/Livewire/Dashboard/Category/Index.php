f9c41b6a4f0c3b95ca0181e23904b40557a6ac3c
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2024-09-02 06:14:00 -0400

    componentes y estilo

--- app/Livewire/Dashboard/Category/Index.php
+++ app/Livewire/Dashboard/Category/Index.php
@@ -10,14 +10,23 @@ use Livewire\WithPagination;
 class Index extends Component
 {
     use WithPagination;
+    public $confirmingDeleteCategory;
+    public $categoryToDelete;
+
+    function selectCategoryToDelete(Category $category){
+        $this->confirmingDeleteCategory = true;
+        $this->categoryToDelete = $category;
+    }
+
     public function render()
     {
         $categories = Category::paginate(2);
         return view('livewire.dashboard.category.index', compact('categories'));
     }
 
-    function delete(Category $category) {
-        $category->delete();
+    function delete(/*Category $category*/) {
+        $this->categoryToDelete->delete();
+        $this->confirmingDeleteCategory=false;
         $this->dispatch('deleted');
     }
 }
--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,37 +1,66 @@
 <div>
 
-    <x-action-message on="created">
-        {{ __('Created category success') }}
-    </x-action-message>
+    <div class="container">
 
-    <x-action-message on="updated">
-        {{ __('Updated category success') }}
-    </x-action-message>
+        <x-action-message on="created">
+            <div class="box-action-message">
+                {{ __('Created category success') }}
+            </div>
+        </x-action-message>
 
-    <form wire:submit.prevent='submit'>
+        <x-action-message on="updated">
+            <div class="box-action-message">
+                {{ __('Updated category success') }}
+            </div>
+        </x-action-message>
 
-        <label for="">Title</label>
-        <input type="text" wire:model.live='title'>
-        @error('title')
-            {{ $message }}
-        @enderror
 
-        <label for="">Text</label>
-        <input type="text" wire:model='text'>
-        @error('text')
-            {{ $message }}
-        @enderror
+        <x-form-section submit='submit'>
 
-        <label for="">Image</label>
-        <input type="file" wire:model='image'>
-        @error('image')
-            {{ $message }}
-        @enderror
+            <x-slot name="title">
+                {{ __('Category') }}
+            </x-slot>
 
-        @if ($category && $category->image)
-            <img class="w-40 my-3" src="{{ $category->getImageUrl() }}" alt="{{ $category->title }}">
-        @endif
+            <x-slot name="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque hic voluptates quaerat accusantium a. Est
+                voluptate voluptatibus necessitatibus a non iure rerum, nesciunt nisi assumenda quaerat nam incidunt ab.
+                Facilis.
+            </x-slot>
 
-        <button type="submit">Send</button>
-    </form>
+            @slot('form')
+
+                <div class="col-span-10 sm:col-span-3">
+                    <x-label for="">Title</x-label>
+                    <x-input type="text" wire:model.live='title' class="w-full" />
+                    @error('title')
+                        {{ $message }}
+                    @enderror
+                </div>
+
+                <div class="col-span-10 sm:col-span-3">
+                    <x-label for="">Text</x-label>
+                    <x-input type="text" wire:model='text' class="w-full" />
+                    @error('text')
+                        {{ $message }}
+                    @enderror
+                </div>
+
+                <div class="col-span-10 sm:col-span-3">
+                    <x-label for="">Image</x-label>
+                    <x-input type="file" wire:model='image' class="w-full" />
+                    @error('image')
+                        {{ $message }}
+                    @enderror
+
+                    @if ($category && $category->image)
+                        <img class="w-40 my-3" src="{{ $category->getImageUrl() }}" alt="{{ $category->title }}">
+                    @endif
+                </div>
+            @endslot
+
+            @slot('actions')
+                <x-button type="submit">Send</x-button>
+            @endslot
+        </x-form-section>
+    </div>
 </div>
